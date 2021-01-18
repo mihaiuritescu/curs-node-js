51605230fb79631b44fce4a71b74fc9bfb7a01f7
Mihai Uritescu <mihai.uritescu@axten.ro>
2021-01-18 12:51:02 +0200

    handlingError parameters

--- config/handlingError.js
+++ config/handlingError.js
@@ -4,12 +4,12 @@ module.exports = {
   handlingError: handlingError
 }
 
-function handlingError (app) {
-  app.use(function(err, result) {
-    console.log("error middleware", err);
-    return result.status(err.statusCode || 400).json({
-      status: "error",
-      message: err && err.message || "Default error message"
-    })
-  })  
+function handlingError(app) {
+  app.use(function(err, req, res, next) {
+      console.log("error middleware", err);
+      return res.status(err.statusCode || 400).json({
+          status: "error",
+          message: err && err.message || "Default error message"
+      })
+  })
 }
